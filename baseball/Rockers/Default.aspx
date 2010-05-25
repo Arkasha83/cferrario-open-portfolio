0d04297566c9968fef814baad677c168bc42faaa
chris_ferrario <chris_ferrario@hotmail.com>
2010-05-25 05:01:48 +0000

    +baseball

---
+++ baseball/Rockers/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="baseball_Rockers_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Rockers Stats</title>
+    <link href="gridview.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:GridView ID="GridView1" runat="server"  
+        GridLines="None"   
+        CssClass="mGrid"  
+        AlternatingRowStyle-CssClass="alt" Caption="Hitting Stats"
+        >
+<AlternatingRowStyle CssClass="alt"></AlternatingRowStyle>
+        </asp:GridView>
+        <asp:GridView ID="GridView2" runat="server"
+        GridLines="None"   
+        CssClass="mGrid"  
+        AlternatingRowStyle-CssClass="alt" Caption="Pitching Stats"
+        >
+            <AlternatingRowStyle CssClass="alt" />
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ baseball/Rockers/Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Data.OleDb;
+
+public partial class baseball_Rockers_Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        String sourceFile = Server.MapPath("./StatSeasonRockers-2010.xls");
+        // Create connection string variable. Modify the "Data Source"
+        // parameter as appropriate for your environment.
+        String sConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;" +
+            "Data Source=" + sourceFile + ";" +
+            "Extended Properties=Excel 8.0;";
+
+        // Create connection object by using the preceding connection string.
+        OleDbConnection DBConnection = new OleDbConnection(sConnectionString);
+
+        // Open connection with the database.
+        DBConnection.Open();
+
+        string SQLString = "Select * from [Totals$B5:AA30]";
+        OleDbCommand DBCommand = new OleDbCommand(SQLString, DBConnection);
+        IDataReader DBReader = DBCommand.ExecuteReader();
+        GridView1.DataSource = DBReader;
+        GridView1.DataBind();
+
+        string SQLString2 = "Select * from [Totals$B36:AA47]";
+        OleDbCommand DBCommand2 = new OleDbCommand(SQLString2, DBConnection);
+        IDataReader DBReader2 = DBCommand2.ExecuteReader();
+        GridView2.DataSource = DBReader2;
+        GridView2.DataBind();
+
+        DBReader.Close();
+        DBConnection.Close();
+    }
+}
+
+
+
+ 
